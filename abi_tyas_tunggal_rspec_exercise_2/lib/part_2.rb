9d857df51ec1f0c1a6e16ccbbaa4f8da75087a11
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-18 19:22:33 +1000

    Finished rspec exercises 2

--- abi_tyas_tunggal_rspec_exercise_2/lib/part_2.rb
+++ abi_tyas_tunggal_rspec_exercise_2/lib/part_2.rb
@@ -2,3 +2,22 @@ def palindrome?(word)
   word.each_char.with_index { |char, i| return false if char != word[-i-1] }
   true
 end
+
+def substrings(word)
+  length = word.length
+  arr = []
+  i = 0
+  while i < length
+    j = i
+    while j < length
+      arr << word[i..j]
+      j += 1
+    end
+    i += 1
+  end
+  arr
+end
+
+def palindrome_substrings(word)
+  substrings(word).select { |substring| palindrome?(substring) && substring.length > 1}
+end
