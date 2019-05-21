729384a446d20945bf71e1da6dd7d7588504d5a5
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-21 20:48:36 +1000

    Finished aba_translate method in blocks project

--- blocks_project/lib/part_1.rb
+++ blocks_project/lib/part_1.rb
@@ -9,3 +9,15 @@ end
 def count_positive_subarrays(array)
   array.count { |subarray| subarray.sum > 0 }
 end
+
+def aba_translate(string)
+  new_string = ""
+  string.each_char do |char|
+    if "aeiou".include?(char)
+      new_string += char + "b" + char
+    else
+      new_string += char
+    end
+  end
+  new_string
+end
