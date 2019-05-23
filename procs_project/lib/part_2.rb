2d4301689ed43fc63efbec60883f0ce85d5c381d
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-23 12:27:46 +1000

    Finished and_selector method in blocks project

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -8,3 +8,16 @@ def word_changer(sentence, &prc)
   new_sentence = sentence.split(" ").map { |word| prc.call(word) }
   new_sentence.join(" ")
 end
+
+def greater_proc_value(number, prc1, prc2)
+  prc1_value = prc1.call(number)
+  prc2_value = prc2.call(number)
+  return prc1_value if prc1_value > prc2_value
+  prc2_value
+end
+
+def and_selector(array, prc1, prc2)
+  new_array = []
+  array.each { |ele| new_array << ele if prc1.call(ele) && prc2.call(ele) }
+  new_array
+end
