3e3620dee7904da8732e81074b94113228aa2c6c
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-23 12:30:45 +1000

    Finished blocks project

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -21,3 +21,15 @@ def and_selector(array, prc1, prc2)
   array.each { |ele| new_array << ele if prc1.call(ele) && prc2.call(ele) }
   new_array
 end
+
+def alternating_mapper(array, prc1, prc2)
+  new_array = []
+  array.each_with_index do |ele, i|
+    if i.even?
+      new_array << prc1.call(ele)
+    else
+      new_array << prc2.call(ele)
+    end
+  end
+  new_array
+end
