11bf4edfb2286a3198c38e46256d552041894585
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-23 12:10:31 +1000

    Finished word_changer method in blocks project

--- procs_project/lib/part_2.rb
+++ procs_project/lib/part_2.rb
@@ -3,3 +3,8 @@ def reverser(string, &prc)
   string.each_char { |char| new_string = char + new_string }
   prc.call(new_string)
 end
+
+def word_changer(sentence, &prc)
+  new_sentence = sentence.split(" ").map { |word| prc.call(word) }
+  new_sentence.join(" ")
+end
