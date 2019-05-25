d6f9cc70199c137d3a23d5e27d5bb9c39d9ad719
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-25 18:39:36 +1000

    Finished startup project

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -31,10 +31,10 @@ class Startup
   end
 
   def pay_employee(employee)
-    pay = @salaries[employee.title]
-    if pay <= @funding
-      employee.pay(pay)
-      @funding -= pay
+    salary = @salaries[employee.title]
+    if salary <= @funding
+      employee.pay(salary)
+      @funding -= salary
     else
       raise "Not enough funding to pay #{employee}."
     end
@@ -42,5 +42,25 @@ class Startup
 
   def payday
     @employees.each { |employee| pay_employee(employee) }
+  end
+
+  def average_salary
+    total_pay = 0
+    titles = Hash.new(0)
+    @employees.each { |employee| titles[employee.title] += 1 }
+    titles.each { |title, count| total_pay += salaries[title] * count }
+    total_pay / @employees.length
+  end
+
+  def close
+    @employees = []
+    @funding = 0
+  end
+  
+  def acquire(startup)
+    @funding += startup.funding
+    startup.salaries.each { |title, salary| @salaries[title] = salary if @salaries[title] == nil }
+    @employees += startup.employees
+    startup.close
   end
 end
