9a4bd5aea4a481c943311a3feeb2f325a9266e6e
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-27 16:58:39 +1000

    Finished part 3

--- mastermind_project/lib/mastermind.rb
+++ mastermind_project/lib/mastermind.rb
@@ -1,5 +1,18 @@
 require_relative "code"
 
 class Mastermind
+  def initialize(length)
+    @secret_code = Code.random(length)
+  end
 
+  def print_matches(code)
+    puts "There were #{@secret_code.num_exact_matches(code)} exact matches and #{@secret_code.num_near_matches(code)} near matches."
+  end
+
+  def ask_user_for_guess
+    puts "Enter a code"
+    guess = Code.from_string(gets.chomp)
+    print_matches(guess)
+    @secret_code == guess
+  end
 end
