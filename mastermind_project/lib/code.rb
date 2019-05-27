72a0651a83428875f6ddc0188a19d7547850aecf
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-27 13:45:39 +1000

    Finished part 1

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -1,4 +1,6 @@
 class Code
+  attr_reader :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -6,4 +8,34 @@ class Code
     "Y" => :yellow
   }
 
+  def Code.valid_pegs?(pegs)
+    pegs.all? { |peg| POSSIBLE_PEGS.has_key?(peg.upcase) }
+  end
+
+  def initialize(pegs)
+    if Code.valid_pegs?(pegs)
+      @pegs = pegs.map(&:upcase)
+    else
+      raise "Invalid pegs"
+    end
+  end
+
+  def Code.random(length)
+    pegs = []
+    length.times { pegs << POSSIBLE_PEGS.keys.sample }
+    p pegs
+    Code.new(pegs)
+  end
+
+  def Code.from_string(pegs)
+    Code.new(pegs.split(""))
+  end
+
+  def [](i)
+    @pegs[i]
+  end
+
+  def length
+    @pegs.length
+  end
 end
