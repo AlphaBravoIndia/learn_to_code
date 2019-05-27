a018ae29ed08716578387c690a842969c35f2fce
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-05-27 16:42:18 +1000

    Finished part 2

--- mastermind_project/lib/code.rb
+++ mastermind_project/lib/code.rb
@@ -1,6 +1,4 @@
 class Code
-  attr_reader :pegs
-
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
@@ -8,8 +6,21 @@ class Code
     "Y" => :yellow
   }
 
-  def Code.valid_pegs?(pegs)
+  attr_reader :pegs
+
+  def self.valid_pegs?(pegs)
     pegs.all? { |peg| POSSIBLE_PEGS.has_key?(peg.upcase) }
+  end
+
+  def self.random(length)
+    pegs = []
+    length.times { pegs << POSSIBLE_PEGS.keys.sample }
+    p pegs
+    Code.new(pegs)
+  end
+
+  def self.from_string(pegs)
+    Code.new(pegs.split(""))
   end
 
   def initialize(pegs)
@@ -20,22 +31,27 @@ class Code
     end
   end
 
-  def Code.random(length)
-    pegs = []
-    length.times { pegs << POSSIBLE_PEGS.keys.sample }
-    p pegs
-    Code.new(pegs)
-  end
-
-  def Code.from_string(pegs)
-    Code.new(pegs.split(""))
-  end
-
   def [](i)
     @pegs[i]
   end
 
   def length
     @pegs.length
+  end
+
+  def num_exact_matches(guess)
+    exact_matches = 0
+    guess.pegs.each_with_index { |peg, i| exact_matches += 1 if peg == self[i]}
+    exact_matches
+  end
+
+  def num_near_matches(guess)
+    near_matches = 0
+    guess.pegs.each_with_index { |peg, i| near_matches += 1 if peg != self[i] && @pegs.include?(peg) }
+    near_matches
+  end
+
+  def ==(code)
+    self.pegs == code.pegs
   end
 end
